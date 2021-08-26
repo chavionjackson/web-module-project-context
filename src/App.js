fb1a3bc0bb0f4b4ab90f552584867c9881428021
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-08-25 19:35:31 -0700

    cart context created

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
 import { ProductContext } from "./contexts/ProductContext";
+import { CartContext } from "./contexts/CartContext";
 
 function App() {
   const [products] = useState(data);
@@ -18,19 +19,21 @@ function App() {
   };
 
   return (
-    <ProductContext.Provider value={{products, addItem}}>
-      <div className="App">
-        <Navigation cart={cart} />
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider>
+        <div className="App">
+          <Navigation cart={cart} />
 
-        {/* Routes */}
-        <Route exact path="/">
-          <Products />
-        </Route>
+          {/* Routes */}
+          <Route exact path="/">
+            <Products />
+          </Route>
 
-        <Route path="/cart">
-          <ShoppingCart cart={cart} />
-        </Route>
-      </div>
+          <Route path="/cart">
+            <ShoppingCart cart={cart} />
+          </Route>
+        </div>
+      </CartContext.Provider>
     </ProductContext.Provider>
   );
 }
