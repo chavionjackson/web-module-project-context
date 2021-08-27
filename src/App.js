12a9d90f04feda71d4404e7b2ba5e3ffdf8aed30
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-08-26 18:42:31 -0700

    attempted stretch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 
@@ -12,20 +12,30 @@ import { CartContext } from "./contexts/CartContext";
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
+//   let localCart = localStorage.getItem("cart");
 
   const addItem = (item) => {
     // add the given item to the cart
-    setCart([...cart, item]);
+    const items = [...cart, item];
+    setCart(items);
+    // localStorage.setItem("cart", JSON.stringify(items));
   };
 
   const removeItem = (id) => {
     const newCart = [...cart.filter((items) => items.id !== id)];
     setCart(newCart);
+    // let cartString = JSON.stringify(newCart);
+    // localStorage.setItem("cart", cartString);
   };
+
+//   useEffect(() => {
+//     localCart = JSON.parse(localCart);
+//     localCart && setCart(localCart);
+//   }, []);
 
   return (
     <ProductContext.Provider value={{ products, addItem }}>
-      <CartContext.Provider value={{cart, removeItem}}>
+      <CartContext.Provider value={{ cart, removeItem }}>
         <div className="App">
           <Navigation />
 
